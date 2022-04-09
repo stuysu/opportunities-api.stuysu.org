e03bfed9796f923eb6e6e5879b1e809c045f0362
Ben Pan <ybenp8104@outlook.com>
2022-04-08 22:47:26 -0400

    Attemp at migration

---
+++ src/database/migrations/20220409024632-create-opportunity.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('opportunities', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('opportunities');
+  }
+};
--- src/database/models/opportunity.js
+++ src/database/models/opportunity.js
@@ -1,24 +1,24 @@
 'use strict';
 const {
-    Model
+  Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-    class opportunity extends Model {
-        /**
-         * Helper method for defining associations.
-         * This method is not a part of Sequelize lifecycle.
-         * The `models/index` file will call this method automatically.
-         */
-        static associate(models) {
-
-        }
-    };
-    opportunity.init({
-        title: DataTypes.STRING,
-        description: DataTypes.STRING,
-    }, {
-        sequelize,
-        modelName: 'opportunity',
-    });
-    return opportunity;
+  class opportunity extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  opportunity.init({
+    title: DataTypes.STRING,
+    description: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'opportunity',
+  });
+  return opportunity;
 };
