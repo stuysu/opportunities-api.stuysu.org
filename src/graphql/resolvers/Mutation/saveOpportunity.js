54324b40bb01a8eb83fb45d357cce9c5f3331e7c
YBenPan <ybenp8104@outlook.com>
2023-02-17 22:02:41 -0500

    Forbid duplicate-saving of opportunities

--- src/graphql/resolvers/Mutation/saveOpportunity.js
+++ src/graphql/resolvers/Mutation/saveOpportunity.js
@@ -1,4 +1,17 @@
+import {ForbiddenError} from "../../index";
+
 export default async (_, { opportunityId, userId }, { models: { userOpps } }) => {
+
+	const numExistingPairs = await userOpps.count({
+		where: {
+			userId: userId,
+			opportunityId: opportunityId
+		}
+	});
+	if (numExistingPairs > 0) {
+		throw new ForbiddenError("You have saved this opportunity already!");
+	}
+
 	await userOpps.create({
 		opportunityId: opportunityId,
 		userId: userId
