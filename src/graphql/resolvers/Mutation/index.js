14180081238348cd997783ace4bded9148ca2d67
frank-cywong <90653148+frank-cywong@users.noreply.github.com>
2023-02-28 09:53:48 -0500

    Add unsave opportunity & authentication

--- src/graphql/resolvers/Mutation/index.js
+++ src/graphql/resolvers/Mutation/index.js
@@ -3,6 +3,7 @@ import createOpportunity from "./createOpportunity";
 import editOpportunity from "./editOpportunity";
 import deleteOpportunity from "./deleteOpportunity";
 import saveOpportunity from "./saveOpportunity";
+import unsaveOpportunity from "./unsaveOpportunity";
 import loginWithGoogle from "./loginWithGoogle";
 import logout from "./logout";
 
@@ -12,6 +13,7 @@ export default {
 	editOpportunity,
 	deleteOpportunity,
 	saveOpportunity,
+	unsaveOpportunity,
 	loginWithGoogle,
 	logout
 };
--- src/graphql/resolvers/Mutation/unsaveOpportunity.js
+++ src/graphql/resolvers/Mutation/unsaveOpportunity.js
@@ -1,6 +1,6 @@
 import {ForbiddenError} from "../../index";
 
-export default async (_, { opportunityId, userId }, { models: { userOpps } }) => {
+export default async (_, { opportunityId, userId }, { models: { userOpps } , user, authenticationRequired}) => {
 
 	authenticationRequired();
 	if(user.id != userId){
@@ -13,11 +13,11 @@ export default async (_, { opportunityId, userId }, { models: { userOpps } }) =>
 			opportunityId: opportunityId
 		}
 	});
-	if (numExistingPairs > 0) {
-		throw new ForbiddenError("You have saved this opportunity already!");
+	if (numExistingPairs == 0) {
+		throw new ForbiddenError("You have not saved this opportunity!");
 	}
 
-	await userOpps.create({
+	await userOpps.destroy({
 		opportunityId: opportunityId,
 		userId: userId
 	});
